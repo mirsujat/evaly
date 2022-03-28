138d1d250ace719b97c464f5e015d9871c1178bb
mirsujat <mir.sujat@gmail.com>
2022-03-28 22:36:24 +0600

    Update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,25 +10,25 @@ import { QueryClient, QueryClientProvider } from "react-query";
 const client = new QueryClient();
 
 
-global.fetch = jest.fn(() => 
-  Promise.resolve({
-    json: () => 
-    Promise.resolve({
-      url: "http//example.com",
-      value: [{"0": "hello", "2": "World" }]
-    }),
-  })
-);
+// global.fetch = jest.fn(() => 
+//   Promise.resolve({
+//     json: () => 
+//     Promise.resolve({
+//       url: "http//example.com",
+//       value: [{"0": "hello", "2": "World" }]
+//     }),
+//   })
+// );
 
 
-  test("load data correctly", async () =>{
-    const { getByTestId } =  render(
+  test("render app component", async () => {
+    const { queryByTestId } =  render(
       <QueryClientProvider client={client}>
       <App />
     </QueryClientProvider>
     );
 
-    const content = await getByTestId('content');
+    const content = await queryByTestId('content');
     expect(content).toBeInTheDocument();
   })
 

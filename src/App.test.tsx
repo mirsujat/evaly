6b8f6c4fc1bb8e8e704aae90466dcac72b2df899
mirsujat <mir.sujat@gmail.com>
2022-03-29 22:21:12 +0600

    mir

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,54 +1,39 @@
-import React from 'react';
-import {rest} from 'msw';
-import {setupServer} from 'msw/node';
+
 import { render, screen, act, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom';
-
-
 import App from './App';
+import { useFetchData } from "./Utility/useFetch";
 
 
-import { QueryClient, QueryClientProvider } from "react-query";
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      // ✅ turns retries off
-      retry: false,
-    },
-  },
-})
-
-const server = setupServer(
-  rest.get('/https://fakestoreapi.com/products"', (req, res, ctx) => {
-    return res(ctx.json({'data': 'hello there'}))
-  }),
-)
-
-beforeAll(() => server.listen())
-afterEach(() => server.resetHandlers())
-afterAll(() => server.close())
-
-// global.fetch = jest.fn(() => 
-//   Promise.resolve({
-//     json: () => 
-//     Promise.resolve({
-//       url: "https://fakestoreapi.com/products",
-//       value: "hello"
-//     }),
-//   })
-// );
+jest.mock("./useFetchData", () => ({
+  useFetchData: jest.fn(),
+}))
 
 
-  test("render loading", async () => {
-      render(
-      <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-    );
-    const content = await screen.getByTestId('loading');
-    expect(content).toBeInTheDocument();
-  });
+ describe("render app without error", () =>{
+   beforeEach(() =>{
+     useFetchData.mockImplementation(() => ({}));
+   });
+
+   describe("while loading..", () =>{
+     it("render loader", () =>{
+       useFetchData.mockImplementation(() =>({
+         isLoading: true
+       }));
+
+       const { getByTestId } = render(<App />);
+       expect( getByTestId("loading") ).toBeTruthy();
+     })
+   });
+
+   describe("with an error..", () =>{
+     it("with error", () =>{})
+   });
+
+   describe("render with data", () =>{
+     it("render with data", () =>{})
+   })
+
+ })
   
 
 
